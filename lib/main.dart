5e5bbc0290cc579a54c0b9c4cc771ad51482570c
kijin <kijin@khu.ac.kr>
2025-11-26 15:03:14 +0900

    로그인 화면 추가 및 메인 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sports_app1/login_screen.dart';
 
 // ----------------------------------------------------
 // 대회 데이터 모델
@@ -174,7 +175,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.white,
         ),
       ),
-      home: const CompetitionMapScreen(),
+      home: const LoginScreen(),
     );
   }
 }
